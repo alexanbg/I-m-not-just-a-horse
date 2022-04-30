653a2abaca23754969a2cabc7403de082015b83f
Niksan005 <nikola.j.pavlovski@gmail.com>
2022-04-30 12:19:22 +0300

    addscript pull

---
+++ I'm not just a horse/Assets/Script/HorsePullToGround.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorsePullToGround : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 vector3;
+
+    void Update()
+    {
+        
+    }
+}
