7b346a4ae384990d63fe7b184c861f1cf3810200
Niksan005 <nikola.j.pavlovski@gmail.com>
2022-05-01 15:27:43 +0300

    Add all obstacles

--- I'm not just a horse/Assets/script/HorseMovement.cs
+++ I'm not just a horse/Assets/script/HorseMovement.cs
@@ -13,7 +13,7 @@ public class HorseMovement : MonoBehaviour
     int MaxCharge;
 
     private Rigidbody2D rb;
-    private int dashCharge = 0;
+    private int dashCharge = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,21 +29,21 @@ public class HorseMovement : MonoBehaviour
             if(dashCharge<MaxCharge)
                 dashCharge++;
         }
-        else if(dashCharge > 0){
+        else if(dashCharge > 5){
             Dash(dashCharge, rb.velocity.y);
-            dashCharge = 0;
+            dashCharge = 5;
         }
     }
 
     private void Dash(int _dashCharge, float _velocity){
         double  v  = Math.Round(_velocity,2);
-        if(v == 0.2){
+        if(v == 0.4){
             rb.velocity = new Vector2(0, -_dashCharge);
         }
-        if(v == -0.2){
+        if(v == -0.4){
             rb.velocity = new Vector2(0, _dashCharge);
         }
-        if(v != 0.2 && v != -0.2 ){
+        if(v != 0.4 && v != -0.4 ){
             // FLip
         }
     }
